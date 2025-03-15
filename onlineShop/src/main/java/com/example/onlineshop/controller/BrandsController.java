ac854a1e8a6f519c03c34676e9f617730414a2e8
MilenkovskiTrajche <milenkovskitm@gmail.com>
2025-03-15 21:35:57 +0100

    added sort by

--- onlineShop/src/main/java/com/example/onlineshop/controller/BrandsController.java
+++ onlineShop/src/main/java/com/example/onlineshop/controller/BrandsController.java
@@ -1,8 +1,12 @@
 package com.example.onlineshop.controller;
 
+import com.example.onlineshop.entity.Role;
+import com.example.onlineshop.entity.Users;
+import com.example.onlineshop.repository.UsersRepository;
 import com.example.onlineshop.service.NavBarService;
 import com.example.onlineshop.service.ProductsService;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +17,15 @@ public class BrandsController {
 
     private final ProductsService productsService;
     private final NavBarService navBarService;
+    private final PasswordEncoder passwordEncoder;
+    private final UsersRepository usersRepository;
+
     public BrandsController(ProductsService productsService,
-                            NavBarService navBarService) {
+                            NavBarService navBarService, PasswordEncoder passwordEncoder, PasswordEncoder passwordEncoder1, UsersRepository usersRepository) {
         this.productsService = productsService;
         this.navBarService = navBarService;
+        this.passwordEncoder = passwordEncoder1;
+        this.usersRepository = usersRepository;
     }
 
 
@@ -30,6 +39,10 @@ public class BrandsController {
             model.addAttribute("products", productsService.findAllByBrand_Name(brand_name));
             model.addAttribute("name", brand_name);
         }
+
+        Users user = new Users("milenkovskitm",passwordEncoder.encode("m"),"m@gmai.com","Trajce","Milenkovski",Role.ADMIN);
+        usersRepository.save(user);
+
         navBarService.setupNavbar(model,authentication);
         return "search"; // Make sure this matches the actual template name
     }
--- onlineShop/src/main/java/com/example/onlineshop/controller/CategoriesController.java
+++ onlineShop/src/main/java/com/example/onlineshop/controller/CategoriesController.java
@@ -1,6 +1,7 @@
 package com.example.onlineshop.controller;
 
 import com.example.onlineshop.entity.Categories;
+import com.example.onlineshop.entity.Products;
 import com.example.onlineshop.service.CategoriesService;
 import com.example.onlineshop.service.NavBarService;
 import com.example.onlineshop.service.ProductsService;
@@ -8,6 +9,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 @RequestMapping("/categories")
@@ -26,6 +30,7 @@ public class CategoriesController {
 
     @GetMapping("/{parent_category_Name}")
     public String showCategory(@PathVariable String parent_category_Name,
+                               @RequestParam(required = false) String sort,
                                Model model,
                                Authentication authentication) {
         // Fetch category by name
@@ -35,22 +40,53 @@ public class CategoriesController {
         }
         // Fetch products by category ID
         assert category != null;
-        model.addAttribute("products", productsService.findByCategoryId(category.getId()));
+        List<Products> products = productsService.findByCategoryId(category.getId());
+        if (sort != null) {
+            switch (sort) {
+                case "priceLowToHigh":
+                    products.sort(Comparator.comparing(Products::getPrice));
+                    break;
+                case "priceHighToLow":
+                    products.sort(Comparator.comparing(Products::getPrice).reversed());
+                    break;
+                case "name":
+                    products.sort(Comparator.comparing(Products::getName));
+                    break;
+            }
+        }
+        model.addAttribute("products",products );
         model.addAttribute("name", category.getName());
+        model.addAttribute("sort", sort);
         navBarService.setupNavbar(model, authentication);
-        return "search"; // Make sure this matches the actual template name
+        return "search";
     }
 
 
     @GetMapping("/{parent_category_Name}/{subCategory_name}")
     public String showSubCategory(@PathVariable String parent_category_Name,
                                   @PathVariable String subCategory_name,
+                                  @RequestParam(required = false) String sort,
                                   Model model,
                                   Authentication authentication) {
         Categories parentCategory = categoriesService.findByName(parent_category_Name);
         Categories category = categoriesService.findByNameAndParentCategoryId(subCategory_name, parentCategory.getId());
-        model.addAttribute("products", productsService.findAllByCategory_Id(category.getId()));
+        List<Products> products = productsService.findAllByCategory_Id(category.getId());
+        if (sort != null) {
+            switch (sort) {
+                case "priceLowToHigh":
+                    products.sort(Comparator.comparing(Products::getPrice));
+                    break;
+                case "priceHighToLow":
+                    products.sort(Comparator.comparing(Products::getPrice).reversed());
+                    break;
+                case "name":
+                    products.sort(Comparator.comparing(Products::getName));
+                    break;
+            }
+        }
+        model.addAttribute("products",products);
         model.addAttribute("name", parent_category_Name+"/"+subCategory_name);
+        model.addAttribute("sort", sort);
         navBarService.setupNavbar(model, authentication);
         return "search";
     }
